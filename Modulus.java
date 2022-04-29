350ce202af92241c02065654f302e0a3e0d66f63
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-04-29 22:02:55 +0530

    import java.util.Scanner; class avg {     public static void main(String args[])     {         int a,b;         int avg;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         avg=a%b;         System.out.println(avg);     } }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class avg
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int avg;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        avg=a%b;
+        System.out.println(avg);
+    }
+}
